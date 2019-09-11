866b87b8315088b05322ede1cba9c9ce57859240
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-09-11 15:11:52 -0300

    CompactarTexto

---
+++ CompactarTexto/CompactarTexto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompactarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeiroTexto  = "Oi meu nome Webster vontade Fever e logo vontade eu Webster tenho vontade de falar muito idioma ";
+
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"texto contem : { primeiroTexto.Length} caracteres.");
+
+            primeiroTexto = primeiroTexto.Replace("Webster", "1").Replace("vontade", "2");
+            Console.WriteLine(primeiroTexto);
+            Console.WriteLine($"texto contem : { primeiroTexto.Length} caracteres.");
+            Console.ReadKey();
+
+        }
+    }
+}
