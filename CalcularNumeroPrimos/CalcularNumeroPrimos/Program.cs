baf49bad43d0c87d508363f2a6edc839e31740ad
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-09-11 14:28:20 -0300

    Calcularprimo

---
+++ CalcularNumeroPrimos/CalcularNumeroPrimos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularNumeroPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calcular o numero primo 
+            double numeroPrimo;
+                
+
+
+            Console.WriteLine("informe um numero");
+            if (numeroPrimo % 2 == 0)
+            }
+                
+            }
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //Iniciando uma constante de um numero inteiro
+        const int testeConst = 999;
+        
+        static void Main(string[] args)
+        {
+
+            string testePrimitivo = string.Empty;
+            String outroTestePrimitivo = string.Empty;
+            bool testComp = "123" == "123";
+            Boolean testeComp = "123" == "123";
+
+            var testeFora = string.Empty;
+            testeFora = "teste";
+            var teste1 = "String"; //testo
+            var teste2 = 'C'; //char
+            var teste3 = 8; // int 
+            var teste4 = 3.8; //double
+            var teste5 = 0x0f; //hex 
+
+            //Apresentando a const
+            Console.WriteLine(testeConst);
+            Console.ReadKey();
+        }
+    }
+}
