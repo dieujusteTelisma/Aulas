6fadee4e6f0b57fa35953511f06c8bfa8edbd6b9
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-09-12 10:11:13 -0300

    tilizandoFor

---
+++ LacoDeRepeticao/UtilizandoFor/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace UtilizandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nome = " meu nome é Felipe Junior e a minha mae escolheu colocar junior em meu nome porque achava legal";
+            var countFind = 0;
+
+            Console.WriteLine("Encontre a palavra de até 3 caracteres :");
+            var palavra = Console.ReadLine();
+
+            for (int i = 0; i < (nome.Length  -2) ; i++) // digitando for logo klike no rab duas vezes para criar todo o codigo
+            {
+                var palavraParaComparar = nome[i].ToString() +
+                                          nome[i + 1].ToString() +
+                                          nome[i + 2].ToString();
+                if (palavraParaComparar.Contains(palavra))
+                    countFind++;
+                    
+            }
+            Console.WriteLine($"Quantidade total de '{palavra}'{countFind}");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+//var nome = "Webster Fever";
+
+// for (int i = 0; i< 13; i++) // digitando for logo klike no rab duas vezes para criar todo o codigo
+// {
+//  Console.WriteLine(nome[i]);
+// Console.WriteLine($"{nome[i - 1]} - {nome[i]} - {nome [i + 1]}");
+//  }
+//   Console.ReadKey();
+
+//--------------------------------------------------------------
+/*var nome = "WebsterFever";
+
+         for (int i = 1; i< 11; i++) // digitando for logo klike no rab duas vezes para criar todo o codigo
+           {
+               Console.WriteLine($"{nome[i - 1]} - {nome[i]} - {nome [i + 1]}");
+           }
+           Console.ReadKey();*/
+
+
+
+ //var nome = "FelipeJunior";
+     //   var countFind = 0;
+
+       // for (int i = 1; i < 11; i++) // digitando for logo klike no rab duas vezes para criar todo o codigo
+       // {
+        //    var stringBuilder = $"{nome[i - 1]} - {nome[i]} - {nome[i + 1]}";
+         //   Console.WriteLine(stringBuilder);
+
+       // if (stringBuilder.Contains("u-n"))
+        //        countFind++;
+       // }
+      //  Console.WriteLine($"Quantidade total de 'un'{countFind}");
+      //  Console.ReadKey();
+ 
---
+++ LacoDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+
+            while (count < 1000)
+            {
+                Console.WriteLine($"Numero : {count}");
+                count++; 
+                //count += 1; este faz mesma coisa que o count++ so que posso contar em 2 em 4 etc
+            }
+
+            Console.WriteLine("terminou o loop");
+            Console.ReadKey();
+
+            // tirando o console.write ele vai muito mais rapido porque ele nao esta reagindo com o console
+        }
+    }
+}
