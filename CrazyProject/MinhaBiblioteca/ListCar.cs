fc3ef51bf3a7865134ceb69a9be5746570664e23
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-09-18 16:25:43 -0300

    AcessandoDllDoAmiguinho

---
+++ CrazyProject/MinhaBiblioteca/ListCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaBiblioteca
+{
+    public class ListCar
+    {
+        public static void ListaCar()
+
+        {
+            var minhaLista = new List<string>();
+
+            minhaLista.Add("Toyota");
+            minhaLista.Add("Ferrari");
+            minhaLista.Add("Lamboghini");
+            minhaLista.Add("Jeep");
+            minhaLista.ForEach(i => Console.WriteLine(i)); // i  significa item da lista 
+
+
+        }
+    }
+}
