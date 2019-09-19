fc6ceed10e67edc94dc6ec22e2c3fec9a8be72eb
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-09-19 17:25:47 -0300

    inhaQuartaClase1

--- MinhaPrimeiraClass/MinhaQuartaClase1/Program.cs
+++ MinhaPrimeiraClass/MinhaQuartaClase1/Program.cs
@@ -23,8 +23,12 @@ namespace MinhaQuartaClase1
         static List<Pessoa> listaPessoa = new List<Pessoa>();
 
         static void Main(string[] args)
-        {
 
+
+        {
+            Lambda();
+
+            List<Pessoa> pessoas = new List<Pessoa>();
             Console.WriteLine("Digite 1 para inserir. ");
             //Console.WriteLine("Digite 2 para listar. ");
             var opcao = Console.ReadLine();
@@ -70,9 +74,61 @@ namespace MinhaQuartaClase1
             listaPessoa.Add(objetoPessoa);
         }
 
+
         public static void listar()
         {
             listaPessoa.ForEach(ob => Console.WriteLine($"Nome {ob.Nome} Idade: {ob.Idade} Sexo: {ob.Sexo} Altura: {ob.Altura}"));
         }
+
+        public static void Lambda()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Gabriel";
+            p1.Sexo = 'M';
+            p1.Altura = 2.01;
+            p1.Idade = 29;
+            pessoas.Add(p1);
+
+            Pessoa p2 = new Pessoa();
+            p2.Nome = "Elóra";
+            p2.Idade = 21;
+            p2.Sexo = 'F';
+            p2.Altura = 1.53;
+            pessoas.Add(p2);
+
+            Pessoa p3 = new Pessoa();
+            p3.Nome = "Ronaldo";
+            p3.Idade = 15;
+            p3.Sexo = 'M';
+            p3.Altura = 1.6;
+            pessoas.Add(p3);
+
+            List<Pessoa> menoresIdade = new List<Pessoa>();
+
+            foreach (Pessoa p in pessoas)
+            {
+                if (p.Idade < 18)
+                {
+                    menoresIdade.Add(p);
+                }
+            }
+            //Lambda = Where/OrderBy/FirstOrDefault
+            pessoas.Where(c => c.Altura > 2 && c.Nome.ToUpper().StartsWith("E"));
+
+            List<Pessoa> pessoasOrdenadasPorAltura = pessoas.OrderByDescending(p => p.Altura).ThenBy(c => c.Idade).ToList();
+
+            //  pessoas.Where(pes => pes.Idade < 18);
+
+
+            var listaMaiores = pessoas.Where(pes => pes.Idade > 18).ToList();
+
+            listaMaiores.ForEach(ob => Console.WriteLine($"Nome: {ob.Nome} "));
+
+
+            menoresIdade.ForEach(ob => Console.WriteLine($"Menor Nome: {ob.Nome} "));
+
+        }
+
     }
 }
