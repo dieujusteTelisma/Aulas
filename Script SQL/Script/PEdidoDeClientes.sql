4366145a079ea3f56a59e03c24b9e22be739af86
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-10-02 17:08:13 -0300

    ExercicioDePedidos

---
+++ Script SQL/Script/PEdidoDeClientes.sql
@@ -0,0 +1,18 @@
+﻿select * from clientes;
+select * from Pedidos;
+
+select cli.* from Clientes cli
+		inner join Pedidos ped on cli.id = ped.ClienteId
+where ped.Numero in ('N000000001','N000000005','N000000009')
+
+select * from Pedidos ped
+where ped.ClienteId in (select cli.Id from Clientes cli
+		inner join Pedidos ped on cli.id = ped.ClienteId
+where ped.Numero in ('N000000001','N000000005','N000000009'))
+
+
+select * from Pedidos ped
+where ped.ClienteId in (select cli.Id from Clientes cli
+		inner join Pedidos ped on cli.id = ped.ClienteId
+where ped.Numero in ('N000000001','N000000005','N000000009'))
+order by ped.Valor desc
