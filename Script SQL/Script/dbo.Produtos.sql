bba7aebab6a4e50bdae4d532db302df41ab99819
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-10-07 10:59:56 -0300

    Exercio extra

---
+++ Script SQL/Script/dbo.Produtos.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Produtos] (
+    [Id]               INT         IDENTITY (1, 1) NOT NULL,
+    [Nome]             NCHAR (100) NOT NULL,
+    [Valor]            MONEY       NOT NULL,
+    [Ativo]            BIT         DEFAULT ((1)) NULL,
+    [UsuarioCriacao]   INT         DEFAULT ((0)) NULL,
+    [UsuarioAlteracao] INT         DEFAULT ((0)) NULL,
+    [DataCriacao]      DATETIME    DEFAULT (getdate()) NULL,
+    [DataAlteracao]    DATETIME    DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+);
+
