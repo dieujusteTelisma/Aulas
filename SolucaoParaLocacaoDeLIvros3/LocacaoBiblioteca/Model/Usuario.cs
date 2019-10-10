a263a7c835b5b34b18c5ce0de98153327ee63b25
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-10-10 00:12:47 -0300

    tarefa 10 de 2019

---
+++ SolucaoParaLocacaoDeLIvros3/LocacaoBiblioteca/Model/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoBiblioteca.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; } = true;
+        public int UsuarioCriacao { get; set; } = 1;
+        public int UsuarioAlteracao { get; set; } = 1;
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public DateTime DataAlteracao { get; set; } = DateTime.Now;
+    }
+}
