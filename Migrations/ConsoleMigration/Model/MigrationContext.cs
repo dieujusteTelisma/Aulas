f9a5c16cae0fd5803d02819b73417803847b2735
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-10-30 14:03:57 -0300

    conexao com banco

---
+++ Migrations/ConsoleMigration/Model/MigrationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ConsoleMigration.Model
+{
+    public class MigrationContext : DbContext  // install last version dando control ponto quando aparece oerro 
+    {
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Migrations/Migrations/Model/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Migrations.Model
+{
+    // nesta classe de web. framework escolhemos o Empty´para acriacao da solucao 
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
