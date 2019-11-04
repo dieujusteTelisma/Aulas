b505e5593892825e7ad13dada05c1fd5cd1d721c
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-11-04 18:15:32 -0200

    webApiCOre

---
+++ DensidadeObjeto/DensidadeObjeto/Model/Objeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DensidadeObjeto.Model
+{
+    public class Objeto
+    {
+        [Key]
+        public int id { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Massa { get; set; }
+
+    }
+}
--- WebApiNetCore/WebApiNetCore/Model/Bicicleta.cs
+++ WebApiNetCore/WebApiNetCore/Model/Bicicleta.cs
@@ -8,8 +8,11 @@ namespace WebApiNetCore.Model
 {
     // para criar um api netcore temos que escolher framework net core dar nome depois procurar emcima o numeor 3.0 e desmarcar as confugurations
     //  temos que ir no tool nugetsegunda option e installin entity framework core .sqlserver e sedserver.in mormery
+    
     // depois fomos no startup para acrecentar  :services.AddDbContext<WebApiContext>(opt =>
     //  opt.UseInMemoryDatabase("DBConect")); na linha 29 
+
+    // depois devemos ir no controller para clicar no add / new scaffold item / Api controller with actions , empty entity framework
     //depois fomos no launchSetting.json para acresentar : "Api/Bicicletas" na linha 15 
 
 // depois fomos na bicicleta controller na linha 85 para acresentat o nameof
